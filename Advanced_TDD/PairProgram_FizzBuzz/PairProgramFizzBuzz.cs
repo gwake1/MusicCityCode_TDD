a1b1bb4618cdaab1c82ebc12d7e10438aced6a9c
Gerald Wakefield <Gerald.Wakefield@ingramcontent.com>
2019-09-09 09:09:10 -0500

    Pair Programm

---
+++ Advanced_TDD/PairProgram_FizzBuzz/PairProgramFizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_TDD.PairProgram_FizzBuzz
+{
+    public class PairProgramFizzBuzz
+    {
+        public static string Speak(int v)
+        {
+            if(v % 3 == 0 && v % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if(isFizzy(v))
+            {
+                return "Fizz";
+            }
+            if(v % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return v.ToString();
+        }
+
+        private static bool isFizzy(int input)
+        {
+            return input % 3 == 0;
+        }
+    }
+}
---
+++ Advanced_TDD_Test/PairProgramFizzBuzzer_Test/PairProgramFizzBuzzerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Advanced_TDD.PairProgram_FizzBuzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Advanced_TDD_Test.PairProgramFizzBuzzer_Test
+{
+    [TestFixture]
+    public class PairProgramFizzBuzzerTest
+    {
+        [Test]
+        public void FizzBuzz_Speak_1_ReturnsOne()
+        {
+            // Arrange
+
+            // Act
+            var actual = PairProgramFizzBuzz.Speak(1);
+
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void FizzBuzz_Speak_3_ReturnsFizz()
+        {
+            // Arrange
+
+            // Act
+            var actual = PairProgramFizzBuzz.Speak(3);
+
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void FizzBuzz_Speak_5_ReturnsBuzz()
+        {
+            // Arrange
+
+            // Act
+            var actual = PairProgramFizzBuzz.Speak(5);
+
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void FizzBuzz_Speak_2_ReturnsTwo()
+        {
+            // Arrange
+
+            // Act
+            var actual = PairProgramFizzBuzz.Speak(2);
+
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void FizzBuzz_Speak_15_ReturnsFizzBuzz()
+        {
+            // Arrange
+
+            // Act
+            var actual = PairProgramFizzBuzz.Speak(15);
+
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("FizzBuzz"));
+        }
+    }
+}
